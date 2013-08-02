016b6b025f42a1d41ac37a7d6c65dfae02402195
unknown <allenf@DATPCDI032.digiterre.com>
2013-08-02 13:09:57 +0100

    adding files

---
+++ devApps.ps1
@@ -0,0 +1,30 @@
+# Dev apps to install
+
+# Visual studio stuff
+cinstm VisualStudio2013Ultimate
+cinstm VisualStudio2012Ultimate
+
+# Visual studio add-ins
+cinstm xUnit.vs2012
+cinstm WebEssentials2012
+cinstm resharper -Version 7.1.3000.2254 # Use only resharper if you want the latests. This is what I have a license for
+cinstm typescript
+cinstm typescript.vs
+cinstm SpecFlow
+
+# databases and tools
+
+# browsers
+cinstm Firefox
+cinstm GoogleChrome
+
+# Other tools
+cinstm git
+cinstm notepadplusplus
+cinstm winmerge
+cinstm Console2
+cinstm 7zip
+cinstm nodejs.install
+cinstm fiddler
+cinstm linqpad4
+
