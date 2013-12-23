d66f96c9dc6d0d67fab83a32df06ce99fea70c80
halcharger <allen.firth@gmail.com>
2013-12-23 15:33:16 +0000

    split out essential installs for new win 8 machines and optional installs into a seperate file

--- win8_1-BoxStarterHomeMachineOptionalInstalls.ps1
+++ win8_1-BoxStarterHomeMachineOptionalInstalls.ps1
@@ -4,22 +4,22 @@ Enable-RemoteDesktop
 # Dev apps to install
 
 # Visual studio stuff
-cinstm VisualStudio2013Ultimate
+cinstm VisualStudio2012Ultimate
 
 # Visual studio add-ins
+cinstm xUnit.vs2012
 cinstm WebEssentials2012
-cinstm resharper
+cinstm typescript
+cinstm typescript.vs
+cinstm SpecFlow
 
 # browsers
-cinstm GoogleChrome
+cinstm Firefox
 
 # Other tools
-cinstm git
-cinstm notepadplusplus
-cinstm 7zip
-cinst winrar
-cinst foxitreader
-cinst vlc
+cinstm winmerge
+cinstm Console2
+cinstm nodejs.install
+cinstm fiddler
+cinstm linqpad4
 
-# gaming
-cinst origin
