abb14d418cb42cff4e6977abf5c8b233d6f4bfca
halcharger <allen.firth@gmail.com>
2013-12-23 13:22:26 +0000

    adding new Win 8.1 BoxStarter script

---
+++ win8_1-BoxStarterHomeMachineScript.ps1
@@ -0,0 +1,33 @@
+Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+Enable-RemoteDesktop
+
+# Dev apps to install
+
+# Visual studio stuff
+cinstm VisualStudio2013Ultimate
+cinstm VisualStudio2012Ultimate
+
+# Visual studio add-ins
+cinstm xUnit.vs2012
+cinstm WebEssentials2012
+cinstm resharper
+cinstm typescript
+cinstm typescript.vs
+cinstm SpecFlow
+
+# browsers
+cinstm Firefox
+cinstm GoogleChrome
+
+# Other tools
+cinstm git
+cinstm notepadplusplus
+cinstm winmerge
+cinstm Console2
+cinstm 7zip
+cinstm nodejs.install
+cinstm fiddler
+cinstm linqpad4
+
+# non Dev stuff
+cinst origin
